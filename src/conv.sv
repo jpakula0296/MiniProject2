eaaf212bef497baf93f6798cfe3ddeec010a7dec
Dave Marnocha <dmarnocha@wisc.edu>
2018-09-21 14:09:27 -0500

    forgot to save conv.sv

--- src/conv.sv
+++ src/conv.sv
@@ -2,11 +2,16 @@ module conv(
 	
 );
 
+// filters
 int sobel1[3][3];
 int sobel2[3][3];
 
 assign sobel1 = {{-1, 0, 1},
 		 {-2, 0, 2},
 		 {-1, 0, 1}};
+
+assign sobel2 = {{-1, 2, -1},
+		 {0, 0, 0},
+		 {1, 2, 1}};
 
 endmodule
