f76e9b92b979f93b8c74fc7d309dce2b263d23bc
Dave Marnocha <dmarnocha@wisc.edu>
2018-09-24 20:18:23 -0500

    commented test bench

--- src/imgproc_tb.sv
+++ src/imgproc_tb.sv
@@ -1,6 +1,5 @@
 module imgproc_tb();
 
-
 // inputs we want to manipulate
 reg clk, rst, sw;
 reg [11:0] data_in;
@@ -11,10 +10,11 @@ reg [15:0] x_cont_in, y_cont_in;
 wire [11:0] oRed, oGreen, oBlue;
 wire data_valid_out;
 
-integer x, y;
-
 parameter WIDTH = 960;
 parameter HEIGHT = 1280;
+
+// loop variables
+integer x, y;
 
 // instantiate DUT
 imgproc iDUT(
@@ -37,20 +37,25 @@ always
 initial begin
 	clk = 0;
 	rst = 0;
-	x_cont_in = 0;
-	y_cont_in = 0;
-	data_valid_in = 1;
+	x_cont_in = 0; // start w/ col = 0
+	y_cont_in = 0; // start w/ row = 0
+	data_valid_in = 1; // set data as always valid
 	sw = 1;
 	#20
+	
+	// start
 	rst = 1;
+
+	// iterate through each row and column
 	for (y = 0; y < WIDTH; y = y+1) begin
 		for (x = 0; x < HEIGHT; x = x+1) begin
+			// update the input on each clock cycle
 			@(posedge clk) begin
 				x_cont_in = x;
 				y_cont_in = y;
+				// arbitrary data, we just need to see that it's being passed through the module correctly
 				data_in = {y_cont_in[5:0], x_cont_in[5:0]};
 			end
-
 		end
 	end
 	data_valid_in = 0;
@@ -59,4 +64,4 @@ initial begin
 	
 end
 
- endmodule
+endmodule
